0448d83582642c586e9d34e72d47a3c2ab1a99a8
Bo Lee <bolee@zipcoders-MacBook-Pro-7.local>
2018-03-16 15:00:39 -0400

    Complete

--- src/main/java/HamletParser.java
+++ src/main/java/HamletParser.java
@@ -1,6 +1,8 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by thook on 10/7/15.
@@ -8,9 +10,12 @@ import java.util.Scanner;
 public class HamletParser {
 
     private String hamletData;
+    private Pattern pattern;
+    private Matcher matcher;
 
     public HamletParser(){
         this.hamletData = loadFile();
+
     }
 
     private String loadFile(){
@@ -34,6 +39,68 @@ public class HamletParser {
 
     public String getHamletData(){
         return hamletData;
+    }
+
+
+    public void changeHoratioToTariq(){
+
+        String horatio = "(Horatio|HORATIO)";
+
+        pattern = Pattern.compile(horatio);
+
+        matcher = pattern.matcher(hamletData);
+        hamletData = matcher.replaceAll("Tariq");
+
+    }
+
+
+    public void changeHamletToLeon() {
+        String hamlet = "(Hamlet| HAMLET)";
+
+        pattern = Pattern.compile(hamlet);
+        matcher = pattern.matcher(hamletData);
+
+        hamletData = matcher.replaceAll("Leon");
+
+    }
+        public String printOut(){
+        System.out.println(this.hamletData);
+        return this.hamletData;
+    }
+
+
+    public static void main(String[] args) {
+        HamletParser hamletParser = new HamletParser();
+        hamletParser.changeHamletToLeon();
+        hamletParser.printOut();
+    }
+
+    public boolean findHoratio() {
+        String horatio = "(Horatio|HORATIO)";
+
+        pattern = Pattern.compile(horatio);
+
+        matcher = pattern.matcher(hamletData);
+
+        if(hamletData.contains("(Horatio|HORATIO)")){
+            return true;
+        }
+        return false;
+
+    }
+
+
+    public boolean findHamlet() {
+        String hamlet = "(Hamlet| HAMLET)";
+
+        pattern = Pattern.compile(hamlet);
+        matcher = pattern.matcher(hamletData);
+
+        if (hamletData.contains("(Hamlet| HAMLET)")) {
+            return true;
+        }
+        return false;
+
     }
 
 }
