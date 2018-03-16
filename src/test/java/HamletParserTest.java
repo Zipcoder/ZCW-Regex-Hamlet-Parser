28d611c047df4ddb5563850fe03bd253b07eecd9
Kaitrina High <kaitrinahigh@zipcoders-MacBook-Pro-81.local>
2018-03-16 13:28:29 -0400

    Done

--- src/test/java/HamletParserTest.java
+++ src/test/java/HamletParserTest.java
@@ -1,5 +1,9 @@
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static org.junit.Assert.*;
 
@@ -9,17 +13,35 @@ public class HamletParserTest {
 
     @Before
     public void setUp() {
-        this.hamletParser = new HamletParser();
-        this.hamletText = hamletParser.getHamletData();
+//        this.hamletParser = new HamletParser();
+//        this.hamletText = hamletParser.getHamletData();
     }
 
     @Test
     public void testChangeHamletToLeon() {
+        String inputLine = "Change Hamlet to Leon. Change Hamlet to Leon.";
+        String[] searchStr = {"Hamlet", "Horatio"};
+        String[] repStr = {"Leon", "Tariq"};
+        String fileName = "test.txt";
+        HamletParser test = new HamletParser(fileName, searchStr, repStr);
+        String actual = test.getHamletData();
+        String expect = "Change Leon to Leon. Change Leon to Leon.\n";
+        Assert.assertEquals(expect, actual);
     }
 
     @Test
     public void testChangeHoratioToTariq() {
+        String inputLine = "Change Horatio to Tariq. Change Horatio to Tariq.";
+        String[] searchStr = {"Hamlet", "Horatio"};
+        String[] repStr = {"Leon", "Tariq"};
+        String fileName = "test.txt";
+        HamletParser test = new HamletParser(fileName, searchStr, repStr);
+        String actual = test.getHamletData();
+        String expect = "Change Leon to Leon. Change Leon to Leon.\n" +
+                "Change Tariq to Tariq. Change Tariq to Tariq.\n";
+        Assert.assertEquals(expect, actual);
     }
+
 
     @Test
     public void testFindHoratio() {
@@ -29,3 +51,5 @@ public class HamletParserTest {
     public void testFindHamlet() {
     }
 }
+
+
