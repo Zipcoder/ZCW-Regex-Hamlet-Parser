2daa3d4eb458d502ecb5fb170e65249e561a24d8
Anthony Jordan <Kain235@gmail.com>
2018-03-16 11:04:00 -0400

    Finished lab

--- src/main/java/HamletParser.java
+++ src/main/java/HamletParser.java
@@ -1,6 +1,10 @@
+import com.sun.org.apache.xerces.internal.impl.xpath.regex.Match;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by thook on 10/7/15.
@@ -30,6 +34,29 @@ public class HamletParser {
         }
 
         return result.toString();
+    }
+
+    public void fileChanger(){
+        hamletChanger();
+        horatioChanger();
+    }
+
+    private void hamletChanger(){
+        Pattern hamletPatternLowerCase = Pattern.compile("Hamlet");
+        Matcher hamletMatcherLowerCase = hamletPatternLowerCase.matcher(hamletData);
+        hamletData = hamletMatcherLowerCase.replaceAll("Leon");
+        Pattern hamletPatternUpperCase = Pattern.compile("HAMLET");
+        Matcher hamletMatcherUpperCase = hamletPatternUpperCase.matcher(hamletData);
+        hamletData = hamletMatcherUpperCase.replaceAll("LEON");
+    }
+
+    private void horatioChanger(){
+        Pattern horatioPatterLowerCase = Pattern.compile("Horatio");
+        Matcher horatioMatcherLowerCase = horatioPatterLowerCase.matcher(hamletData);
+        hamletData = horatioMatcherLowerCase.replaceAll("Tariq");
+        Pattern horatioPatterUpperCase = Pattern.compile("HORATIO");
+        Matcher horatioMatcherUpperCase = horatioPatterUpperCase.matcher(hamletData);
+        hamletData = horatioMatcherUpperCase.replaceAll("TARIQ");
     }
 
     public String getHamletData(){
