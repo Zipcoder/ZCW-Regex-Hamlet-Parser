c6e92f2e2fa9fb1b86301f3037b2ba73ca2740da
April Rivera <aprilrivera@zipcoders-MacBook-Pro-2.local>
2018-03-16 17:05:21 -0400

    Done?

--- src/main/java/HamletParser.java
+++ src/main/java/HamletParser.java
@@ -1,6 +1,8 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by thook on 10/7/15.
@@ -9,31 +11,61 @@ public class HamletParser {
 
     private String hamletData;
 
-    public HamletParser(){
+    public HamletParser() {
         this.hamletData = loadFile();
     }
 
-    private String loadFile(){
+    private String loadFile() {
         ClassLoader classLoader = getClass().getClassLoader();
         File file = new File(classLoader.getResource("hamlet.txt").getFile());
         StringBuilder result = new StringBuilder("");
 
-        try(Scanner scanner = new Scanner(file)){
-            while(scanner.hasNextLine()){
+        try (Scanner scanner = new Scanner(file)) {
+            while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
                 result.append(line).append("\n");
             }
 
             scanner.close();
-        }catch(IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
-
         return result.toString();
     }
 
-    public String getHamletData(){
+    public String getHamletData() {
         return hamletData;
     }
 
+
+    public void changeIfHamlet() {
+        Pattern p = Pattern.compile("(Hamlet| HAMLET).*");
+        Matcher m = p.matcher(hamletData);
+        if (m.find()) {
+            hamletData = m.replaceAll("Leon");
+        } else if (m.find()){
+            hamletData = m.replaceAll("LEON");
+        }
+        }
+
+    public void changeIfHoratio(){
+        Pattern p = Pattern.compile("(Horatio | HORATIO).*");
+        Matcher m = p.matcher(hamletData);
+        if (m.find()){
+            hamletData = m.replaceAll("Tariq");
+        } else if (m.find()) {
+            hamletData = m.replaceAll("TARIQ");
+        }
+    }
+    public boolean findHamlet(){
+        Pattern p = Pattern.compile("(Hamlet| HAMLET).*");
+        Matcher m = p.matcher(hamletData);
+        return m.find();
+    }
+    public boolean findHoratio(){
+        Pattern p = Pattern.compile("(Horatio | HORATIO).*");
+        Matcher m = p.matcher(hamletData);
+        return m.find();
+    }
 }
+
