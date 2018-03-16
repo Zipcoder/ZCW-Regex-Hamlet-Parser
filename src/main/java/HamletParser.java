2ffd7bad76585f5407c2baefbe78f383adcca208
Carolynn Vansant <cvansant@MacBook-Pro-5.local>
2018-03-16 15:52:25 -0400

    first commit

--- src/main/java/HamletParser.java
+++ src/main/java/HamletParser.java
@@ -1,6 +1,8 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by thook on 10/7/15.
@@ -13,6 +15,10 @@ public class HamletParser {
         this.hamletData = loadFile();
     }
 
+    public String getHamletData(){
+        return hamletData;
+    }
+
     private String loadFile(){
         ClassLoader classLoader = getClass().getClassLoader();
         File file = new File(classLoader.getResource("hamlet.txt").getFile());
@@ -23,17 +29,25 @@ public class HamletParser {
                 String line = scanner.nextLine();
                 result.append(line).append("\n");
             }
-
             scanner.close();
         }catch(IOException e){
             e.printStackTrace();
         }
-
         return result.toString();
     }
 
-    public String getHamletData(){
-        return hamletData;
+    public String changeHamletToLeon(){
+        Pattern pattern1 = Pattern.compile("Hamlet");
+        Matcher matcher1 = pattern1.matcher(loadFile());
+        return matcher1.replaceAll("Leon");
     }
+
+    public String changeHoratioToTariq(){
+        Pattern pattern2 = Pattern.compile("Horatio");
+        Matcher matcher2 = pattern2.matcher(changeHamletToLeon());
+        return matcher2.replaceAll("Tariq");
+    }
+
+
 
 }
--- src/test/java/HamletParserTest.java
+++ src/test/java/HamletParserTest.java
@@ -1,3 +1,4 @@
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -15,17 +16,20 @@ public class HamletParserTest {
 
     @Test
     public void testChangeHamletToLeon() {
+        Assert.assertEquals(true, hamletText.contains("Hamlet"));
+        String replaced = hamletParser.changeHamletToLeon();
+        Assert.assertEquals(true, replaced.contains("Leon"));
+        Assert.assertEquals(false, replaced.contains("Hamlet"));
     }
 
     @Test
     public void testChangeHoratioToTariq() {
+        Assert.assertEquals(true, hamletText.contains("Horatio"));
+        String replaced = hamletParser.changeHoratioToTariq();
+        Assert.assertEquals(true, replaced.contains("Tariq"));
+        Assert.assertEquals(true, replaced.contains("Leon"));
+        Assert.assertEquals(false, replaced.contains("Hamlet"));
+        Assert.assertEquals(false, replaced.contains("Horatio"));
     }
 
-    @Test
-    public void testFindHoratio() {
-    }
-
-    @Test
-    public void testFindHamlet() {
-    }
 }
