b8ffd6c2880895a5e78f8757782c0066a774c6d5
Brian He <brianhe@zipcoders-MacBook-Pro-56.local>
2018-03-16 12:06:19 -0400

    finished

--- src/test/java/HamletParserTest.java
+++ src/test/java/HamletParserTest.java
@@ -1,5 +1,9 @@
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static org.junit.Assert.*;
 
@@ -15,17 +19,41 @@ public class HamletParserTest {
 
     @Test
     public void testChangeHamletToLeon() {
+        hamletParser.changeHamletToLeon();
+        Pattern pattern = Pattern.compile("Hamlet");
+        Pattern pattern2 = Pattern.compile("Leon");
+        Matcher matcher = pattern.matcher(hamletParser.getHamletData());
+        Matcher matcher2 = pattern2.matcher(hamletParser.getHamletData());
+        Assert.assertFalse(matcher.find());
+        Assert.assertTrue(matcher2.find());
     }
 
     @Test
     public void testChangeHoratioToTariq() {
+        hamletParser.changeHoratioToTariq();
+        Pattern pattern = Pattern.compile("Horatio");
+        Pattern pattern2 = Pattern.compile("Tariq");
+        Matcher matcher = pattern.matcher(hamletParser.getHamletData());
+        Matcher matcher2 = pattern2.matcher(hamletParser.getHamletData());
+        Assert.assertFalse(matcher.find());
+        Assert.assertTrue(matcher2.find());
+
     }
 
     @Test
     public void testFindHoratio() {
+        hamletParser.changeHoratioToTariq();
+        Pattern pattern = Pattern.compile("Horatio");
+        Matcher matcher = pattern.matcher(hamletParser.getHamletData());
+        Assert.assertFalse(matcher.find());
+
     }
 
     @Test
     public void testFindHamlet() {
+        hamletParser.changeHamletToLeon();
+        Pattern pattern = Pattern.compile("Hamlet");
+        Matcher matcher = pattern.matcher(hamletParser.getHamletData());
+        Assert.assertFalse(matcher.find());
     }
 }
