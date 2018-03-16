fcfcf70a9986d16b54ff65d872d932b8c625bc3b
Keith Brinker <keithbrinker@zipcoders-MacBook-Pro-2.local>
2018-03-16 15:11:23 -0400

    still working, might have to refactor

--- src/main/java/HamletParser.java
+++ src/main/java/HamletParser.java
@@ -1,16 +1,25 @@
+import com.sun.tools.doclets.formats.html.SourceToHTMLConverter;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by thook on 10/7/15.
  */
 public class HamletParser {
 
+    private HamletParser hamletParser;
     private String hamletData;
+    private String changedNames;
+    private String input;
 
     public HamletParser(){
         this.hamletData = loadFile();
+       // this.hamletParser = new HamletParser();
+       // this.input = hamletParser.changeHamletToLeon();
     }
 
     private String loadFile(){
@@ -30,6 +39,68 @@ public class HamletParser {
         }
 
         return result.toString();
+    }
+
+    public boolean findHoratio(){
+        if (hamletData.contains("HORATIO") || hamletData.contains("Horatio"));
+        return true;
+    }
+
+    public boolean findHamlet(String input){
+        if (hamletData.contains("HAMLET") || hamletData.contains("Hamlet"));
+        return true;
+    }
+
+    public String changeHoratioToTariq(){
+
+        String str = hamletData;
+        Pattern p = Pattern.compile("\\bHoratio\\b");
+        Matcher m = p.matcher(str);
+        String capital = m.replaceAll("Tariq");
+
+        String lower = capital;
+        Pattern p2 = Pattern.compile("\\bHORATIO\\b");
+        Matcher m2 = p2.matcher(lower);
+        String result = m2.replaceAll("TARIQ");
+        return result;
+    }
+
+    public String changeHamletToLeon(){
+
+        String str = hamletData;
+        Pattern p = Pattern.compile("\\bHamlet\\b");
+        Matcher m = p.matcher(str);
+        String capital = m.replaceAll("Leon");
+
+        String lower = capital;
+        Pattern p2 = Pattern.compile("\\bHAMLET\\b");
+        Matcher m2 = p2.matcher(lower);
+        String result = m2.replaceAll("LEON");
+        return result;
+    }
+
+    public String changeBothNames(){
+        String str = hamletData;
+        Pattern p = Pattern.compile("\\bHoratio\\b");
+        Matcher m = p.matcher(str);
+        String capital = m.replaceAll("Tariq");
+
+        String lower = capital;
+        Pattern p2 = Pattern.compile("\\bHORATIO\\b");
+        Matcher m2 = p2.matcher(lower);
+        String resultOne = m2.replaceAll("TARIQ");
+
+        String hamBoy = resultOne;
+        Pattern p3 = Pattern.compile("\\bHamlet\\b");
+        Matcher m3 = p3.matcher(hamBoy);
+        String ham1 = m3.replaceAll("Leon");
+
+        String ham2 = ham1;
+        Pattern p4 = Pattern.compile("\\bHAMLET\\b");
+        Matcher m4 = p4.matcher(ham2);
+        String result = m4.replaceAll("LEON");
+        return result;
+
     }
 
     public String getHamletData(){
--- src/test/java/HamletParserTest.java
+++ src/test/java/HamletParserTest.java
@@ -1,5 +1,9 @@
+import com.sun.tools.doclets.formats.html.SourceToHTMLConverter;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static org.junit.Assert.*;
 
@@ -15,17 +19,29 @@ public class HamletParserTest {
 
     @Test
     public void testChangeHamletToLeon() {
+        System.out.println(hamletParser.changeHamletToLeon());
     }
+
 
     @Test
     public void testChangeHoratioToTariq() {
+        System.out.println(hamletParser.changeHoratioToTariq());
+
     }
 
     @Test
     public void testFindHoratio() {
+        hamletParser.findHamlet(hamletParser.changeHamletToLeon());
+        assertTrue(true);
     }
 
     @Test
     public void testFindHamlet() {
+        hamletParser.findHoratio();
+    }
+
+    @Test
+    public void testChangedBothNames(){
+        System.out.println(hamletParser.changeBothNames());
     }
 }
