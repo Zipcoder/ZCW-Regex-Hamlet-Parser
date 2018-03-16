0f963a3f06a21cda2f6f11f116660ec76a3670a0
Daniel Horowitz <danielhorowitz@zipcoders-MacBook-Pro-80.local>
2018-03-16 14:50:28 -0400

    lab complete

--- src/test/java/HamletParserTest.java
+++ src/test/java/HamletParserTest.java
@@ -1,3 +1,4 @@
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -13,19 +14,63 @@ public class HamletParserTest {
         this.hamletText = hamletParser.getHamletData();
     }
 
+
+
     @Test
     public void testChangeHamletToLeon() {
+        //Given
+        String test = "Hamlet HAMLET HORATIO. Horatio";
+
+        //When
+        String expected = "Leon LEON HORATIO. Horatio";
+        String actual = hamletParser.changeHamletToLeon(test);
+
+        Assert.assertEquals(expected,actual);
+
+
     }
 
     @Test
     public void testChangeHoratioToTariq() {
+        //Given
+        String test = "Hamlet HAMLET HORATIO. Horatio";
+
+        //When
+        String expected = "Hamlet HAMLET TARIQ. Tariq";
+        String actual = hamletParser.changeHoratioToTariq(test);
+
+        Assert.assertEquals(expected,actual);
+
+
     }
 
     @Test
+    public void testChangeALL() {
+        //Given
+        String test = "Hamlet HAMLET HORATIO. Horatio";
+
+        //When
+        String expected = "Leon LEON TARIQ. Tariq";
+        String actual = hamletParser.changeAll(test);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+
+        @Test
     public void testFindHoratio() {
+
+        int expected = 158;
+        int actual = hamletParser.findHoratio();
+        Assert.assertEquals(expected,actual);
+
     }
 
     @Test
     public void testFindHamlet() {
+        int expected = 472;
+        int actual = hamletParser.findHamlet();
+        Assert.assertEquals(expected,actual);
+
     }
 }
