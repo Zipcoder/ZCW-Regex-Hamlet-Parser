127605160834c7fc61176fa3eebd71365258e5e5
Kaitrina High <kaitrinahigh@zipcoders-MacBook-Pro-81.local>
2018-03-16 13:53:11 -0400

    removed stuff

--- src/test/java/HamletParserTest.java
+++ src/test/java/HamletParserTest.java
@@ -8,14 +8,7 @@ import java.util.regex.Pattern;
 import static org.junit.Assert.*;
 
 public class HamletParserTest {
-    private String hamletText;
-    private HamletParser hamletParser;
 
-    @Before
-    public void setUp() {
-//        this.hamletParser = new HamletParser();
-//        this.hamletText = hamletParser.getHamletData();
-    }
 
     @Test
     public void testChangeHamletToLeon() {
